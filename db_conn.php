6c7ea4e9ebbdc74149a8d61af537bef3e0b9afcc
Pipa PlohoiAnimator <janzerikYT@yandex.ru>
2021-10-23 06:18:07 +0500

    make it better

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbHost="localhost";
-$dbUser="root";
-$dbPass="root";
+$dbHost="eu-cdbr-west-01.cleardb.com";
+$dbUser="bb03da1cbb1d04";
+$dbPass="315f7012";
 $dbName="test";
 $dbport="3306";
 $edit_state = false;
@@ -14,24 +14,3 @@ try {
 } catch (PDOException $e) {
     echo "Connection failed: ". $e->getMessage();
 }
-
-//$fname = $_POST['fname'];
-//$phone = $_POST['phone'];
-//$bank = $_POST['bank'];
-//$sql = "CREATE TABLE IF NOT EXISTS telephones (
-//    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-//    fname VARCHAR(40) NOT NULL ,
-//    phone VARCHAR(11) NOT NULL,
-//    bank TEXT NOT NULL,
-//    checked TINYINT(1) DEFAULT 0
-//);";
-//$conn->exec($sql);
-//$pdoQuery = "INSERT INTO `telephones` (`fname`, `phone`, `bank`)
-//VALUES (:fname, :phone, :bank)";
-//$pdoResult = $conn->prepare($pdoQuery);
-//$pdoExec = $pdoResult->execute(array(":fname" => $fname, ":phone"=>$phone, ":bank" => $bank));
-//if($pdoExec) {
-//    echo "Data Inserted";
-//} else {
-//    echo "Data not Inserted";
-//}
