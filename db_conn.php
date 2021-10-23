fa89044ecc6acc1163b5392189a257f0b6a156a8
Pipa PlohoiAnimator <janzerikYT@yandex.ru>
2021-10-23 06:44:40 +0500

    make it better

--- db_conn.php
+++ db_conn.php
@@ -3,7 +3,7 @@
 $dbHost="eu-cdbr-west-01.cleardb.com";
 $dbUser="bb03da1cbb1d04";
 $dbPass="315f7012";
-$dbName="test";
+$dbName="heroku_6c2a5fad4553d00";
 $dbport="3306";
 $edit_state = false;
 
@@ -11,6 +11,14 @@ try {
     $dsn = "mysql:host=$dbHost;dbname=$dbName;port=$dbport";
     $conn = new PDO($dsn, $dbUser, $dbPass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS telephones (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fname TEXT NOT NULL DEFAULT '',
+    phone VARCHAR(11) NOT NULL DEFAULT '',
+    bank TEXT NOT NULL DEFAULT '',
+    checked TINYINT(1) DEFAULT 0
+)";
+    $conn->exec($sql);
 } catch (PDOException $e) {
     echo "Connection failed: ". $e->getMessage();
 }
